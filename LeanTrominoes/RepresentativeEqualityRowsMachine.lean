ce79c0d6c73dae6bd13054479b68f0aef0c9bb7f
Erik Demaine <edemaine@mit.edu>
2026-08-21 06:10:56 -0400

    Parameterize rejected-row configurations

--- LeanTrominoes/RepresentativeEqualityRowsMachine.lean
+++ LeanTrominoes/RepresentativeEqualityRowsMachine.lean
@@ -226,7 +226,8 @@ def finishRowCfg (representative : Bool) (data : TapeData) :=
   cfg .finishRow ⟨none, false, representative⟩ data
 def moveSelectedToForwardCfg := idleCfg .moveSelectedToForward
 def moveSelectedToOutputCfg := idleCfg .moveSelectedToOutput
-def clearRejectedRowCfg := idleCfg .clearRejectedRow
+def clearRejectedRowCfg (representative : Bool) (data : TapeData) :=
+  cfg .clearRejectedRow ⟨none, false, representative⟩ data
 def clearRowIndexCfg := idleCfg .clearRowIndex
 def reverseOutputCfg := idleCfg .reverseOutput
 
