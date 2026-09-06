c3521fa0bb35b1588dcd2a0afc8f85754306ee3d
Erik Demaine <edemaine@mit.edu>
2026-09-06 04:27:19 -0400

    Import descriptor projection semantics in recipe alignment

--- LeanTrominoes/PeriodicOrthocrossingRouteDescriptorOccurrenceSlotCrossingCarrierKeyWordRecipeAlignmentSemantics.lean
+++ LeanTrominoes/PeriodicOrthocrossingRouteDescriptorOccurrenceSlotCrossingCarrierKeyWordRecipeAlignmentSemantics.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Erik Demaine, Stefan Langerman, GPT 5.6
 -/
 import LeanTrominoes.PeriodicOrthocrossingRouteDescriptorOccurrenceSlotCrossingCarrierKeyWordRecipeSemantics
+import LeanTrominoes.PeriodicOrthocrossingRouteDescriptorOccurrenceSlotPairProjectionSemantics
 
 /-! # Alignment of crossing carrier-key recipes and templates -/
 
